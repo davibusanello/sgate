18e6b2c410c9b9ee48ea5e1a061e1ce461b0bcb7
Davi Busanello <contato@davibusanello.net>
2012-11-22 21:35:55 -0200

    start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sgate::Application.config.secret_token = '6bbc1c7db3d95016442a70b5b7715f7c3a512c4ee8519fd375be4d1ecf7261c0ad8dac25973124197559fe012f9f7dbc8d8be498a64c70b8d8f96db99d54aea5'
