8d9c0e9168166b3c15032ab66e778e72874c880c
Davi Busanello <contato@davibusanello.net>
2012-12-13 20:35:16 -0200

    association

--- app/models/fatura.rb
+++ app/models/fatura.rb
@@ -1,3 +1,6 @@
 class Fatura < ActiveRecord::Base
   attr_accessible :cliente_id, :data_vencimento, :nome, :numero_inscricao, :pacote_id, :valor
+
+  belongs_to :Cliente, :class_name => "Cliente", :foreign_key => "cliente_id"
+  belongs_to :Pacote, :class_name => "Pacote", :foreign_key => "pacote_id"
 end
